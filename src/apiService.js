f844792c76b59ba8ae4429d00ec0c0d828808fb8
kanuni <mister.nicto006@gmail.com>
2024-09-05 12:22:13 +0300

    setting url

--- src/apiService.js
+++ src/apiService.js
@@ -1,6 +1,5 @@
 export async function getWeather(city) {
-    const apiKey = "b2f0200ba5b24644a7773504240309";
-    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+    const url = `http://api.weatherapi.com/v1/current.json?key=b2f0200ba5b24644a7773504240309&q=${city}`;
     const response = await fetch(url);
     
     if (!response.ok) {
