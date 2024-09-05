fe34c180299f8ce06c1696291b0ef2daf2d4e1c5
kanuni <mister.nicto006@gmail.com>
2024-09-05 12:38:15 +0300

    url

--- src/apiService.js
+++ src/apiService.js
@@ -1,10 +1,14 @@
+
 export async function getWeather(city) {
-    const url = `http://api.weatherapi.com/v1/current.json?key=b2f0200ba5b24644a7773504240309&q=${city}`;
+    const url = `https://api.weatherapi.com/v1/current.json?key=b2f0200ba5b24644a7773504240309&q=${city}`;
     const response = await fetch(url);
     
     if (!response.ok) {
       throw new Error(`Error fetching weather data: ${response.status}`);
     }
     
+    console.log(response);
     return response.json();
   }
+
+  
